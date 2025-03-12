9c3a2cbbd43462aa36b8375ae3dcdb5f580b7913
mramachandrappa <maheshramachandrappa91@gmail.com>
2025-03-12 20:29:13 +0530

    Test'

--- .github/workflows/apex_name_check.sh
+++ .github/workflows/apex_name_check.sh
@@ -3,48 +3,53 @@
 declare -a INVALID_FILES=()
 APEX_CLS_NAME_CHECK="Passed!"
 
-REGEX_PATTERNS=( 
-            "^A3A_[A-Za-z]+Testing\.cls$"
-            "^ABC_[A-Za-z]+Service\.cls$"
-            "^A3C_[A-Za-z]+Service\.cls$"
-            )
+REGEX_PATTERNS=(
+    "^A3A_[A-Za-z]+Testing\.cls$"
+    "^ABC_[A-Za-z]+Service\.cls$"
+    "^A3C_[A-Za-z]+Service\.cls$"
+)
 
-FILES_CHANGED=($(git diff --name-only --diff-filter=AM origin/main...HEAD -- '*.cls' | xargs -I {} basename {}))
+# Get changed .cls files
+FILES_CHANGED=($(git diff --name-only --diff-filter=AM origin/main...HEAD -- '*.cls'))
 
-echo "Found following .cls files with change \n $FILES_CHANGED"''
+echo -e "Found the following .cls files with changes:\n${FILES_CHANGED[*]}\n"
 
-if [ ${#FILES_CHANGED[@]} -eq 0 ]; then
-    echo "No .cls files found with changes"
-    INVALID_FILES="None!"
+if [[ ${#FILES_CHANGED[@]} -eq 0 ]]; then
+    echo "No .cls files found with changes."
+    echo "INVALID_FILES=None!" >> "$GITHUB_OUTPUT"
+    echo "APEX_CLS_NAME_CHECK=$APEX_CLS_NAME_CHECK" >> "$GITHUB_OUTPUT"
     exit 0
 fi
 
-for FILE_NAME in ${FILES_CHANGED[@]}; do
-    for PATTERN in ${REGEX_PATTERNS[@]}; do
+# Validate naming conventions
+for FILE_PATH in "${FILES_CHANGED[@]}"; do
+    FILE_NAME=$(basename "$FILE_PATH")
+    matched=false
+
+    for PATTERN in "${REGEX_PATTERNS[@]}"; do
         if [[ $FILE_NAME =~ $PATTERN ]]; then
-        matched=true
-        break
-        else
-        matched=false
+            matched=true
+            break
         fi
     done
-    echo $matched
-    if [ "$matched" ]; then
-        echo "$FILE_NAME passed naming convention"
+
+    if $matched; then
+        echo "$FILE_NAME passed naming convention check."
     else
         INVALID_FILES+=("$FILE_NAME")
-        echo "$FILE_NAME has not passed naming convention check!"
+        echo "$FILE_NAME did NOT pass the naming convention check!"
     fi
 done
 
-if [ ${#INVALID_FILES[@]} -eq 0 ]; then
-    INVALID_FILES=None!
-    echo "INVALID_FILES=$INVALID_FILES" >> $GITHUB_OUTPUT
-    echo "APEX_CLS_NAME_CHECK=$APEX_CLS_NAME_CHECK" >> $GITHUB_OUTPUT
+# Output results
+if [[ ${#INVALID_FILES[@]} -eq 0 ]]; then
+    echo "All files passed naming conventions."
+    echo "INVALID_FILES=None!" >> "$GITHUB_OUTPUT"
+    echo "APEX_CLS_NAME_CHECK=$APEX_CLS_NAME_CHECK" >> "$GITHUB_OUTPUT"
 else
-    echo "Found Invalid Files : $INVALID_FILES"
+    echo -e "Invalid Files Found:\n${INVALID_FILES[*]}"
     APEX_CLS_NAME_CHECK="Failed!"
-    echo "INVALID_FILES=$INVALID_FILES" >> $GITHUB_OUTPUT
-    echo "APEX_CLS_NAME_CHECK=$APEX_CLS_NAME_CHECK" >> $GITHUB_OUTPUT
+    echo "INVALID_FILES=${INVALID_FILES[*]}" >> "$GITHUB_OUTPUT"
+    echo "APEX_CLS_NAME_CHECK=$APEX_CLS_NAME_CHECK" >> "$GITHUB_OUTPUT"
     exit 1
 fi
