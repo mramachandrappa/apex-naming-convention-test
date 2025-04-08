177d7e51067fd718b8598548d92118036466d4f1
mramachandrappa <maheshramachandrappa91@gmail.com>
2025-04-08 10:08:40 +0530

    repository variable

--- .github/workflows/apex_name_check.sh
+++ .github/workflows/apex_name_check.sh
@@ -3,9 +3,10 @@
 declare -a INVALID_FILES=()
 APEX_CLS_NAME_CHECK="Passed!"
 
-REGEX_PATTERNS=(
-    "^(A3A|A3C|appomni__AppOmni|BFCC|BFCCQIO|ChangePassword|CMS|Communities|CQP|FQS|ForgotPassword|LoginAs|Microbatch|MyProfilePage|OutcomeReportLWC|PicklistValues|SEIAV|SiteLogin|SiteRegister|Test_SEIAV|UserDeProvisioning)[_a-zA-Z]*\.(cls|cls-meta\.xml)$"
-)
+# REGEX_PATTERNS=(
+#     "^(A3A|A3C|appomni__AppOmni|BFCC|BFCCQIO|ChangePassword|CMS|Communities|CQP|FQS|ForgotPassword|LoginAs|Microbatch|MyProfilePage|OutcomeReportLWC|PicklistValues|SEIAV|SiteLogin|SiteRegister|Test_SEIAV|UserDeProvisioning)[_a-zA-Z]*\.(cls|cls-meta\.xml)$"
+# )
+
 
 # Get changed .cls files
 FILES_CHANGED=($(git diff --name-only --diff-filter=AM origin/main...HEAD -- '*.cls*'))
@@ -24,7 +25,7 @@ for FILE_PATH in "${FILES_CHANGED[@]}"; do
     FILE_NAME=$(basename "$FILE_PATH")
     matched=false
 
-    for PATTERN in "${REGEX_PATTERNS[@]}"; do
+    for PATTERN in "${vars.REGEX_PATTERNS[@]}"; do
         echo "$FILE_NAME" | grep -Ei "$PATTERN" > /dev/null
         if [[ $? -eq 0 ]]; then
             matched=true
