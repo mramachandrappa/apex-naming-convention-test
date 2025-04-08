56c55cec0542d0131b87731a514c8c96091dd4ba
mramachandrappa <maheshramachandrappa91@gmail.com>
2025-04-08 11:24:54 +0530

    repository variable

--- .github/workflows/apex_name_check.sh
+++ .github/workflows/apex_name_check.sh
@@ -4,9 +4,9 @@ declare -a INVALID_FILES=()
 APEX_CLS_NAME_CHECK="Passed!"
 
 # REGEX_PATTERNS is defined in repository variable
-# REGEX_PATTERNS_TEST=(
-#     "^(A3A|A3C|appomni__AppOmni|BFCC|BFCCQIO|ChangePassword|CMS|Communities|CQP|FQS|ForgotPassword|LoginAs|Microbatch|MyProfilePage|OutcomeReportLWC|PicklistValues|SEIAV|SiteLogin|SiteRegister|Test_SEIAV|UserDeProvisioning)[_a-zA-Z]*\.(cls|cls-meta\.xml)$"
-# )
+REGEX_PATTERNS_TEST=(
+    "^(A3A|A3C|appomni__AppOmni|BFCC|BFCCQIO|ChangePassword|CMS|Communities|CQP|FQS|ForgotPassword|LoginAs|Microbatch|MyProfilePage|OutcomeReportLWC|PicklistValues|SEIAV|SiteLogin|SiteRegister|Test_SEIAV|UserDeProvisioning)[_a-zA-Z]*\.(cls|cls-meta\.xml)$"
+)
 
 echo "REGEX_PATTERN is $REGEX_PATTERN"
 
