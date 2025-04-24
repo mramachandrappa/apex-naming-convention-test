00200c8e81cb9adec7899dd983690153e51455cb
mramachandrappa <maheshramachandrappa91@gmail.com>
2025-04-24 20:21:47 +0530

    Test

--- .github/workflows/lwc_name_check.sh
+++ .github/workflows/lwc_name_check.sh
@@ -34,11 +34,11 @@ done
 
 # Output results
 if [[ ${#INVALID_FILES[@]} -eq 0 ]]; then
-    echo "\nAll LWC components passed naming conventions."
+    echo "\n All LWC components passed naming conventions."
     echo "INVALID_FILES=None!" >> "$GITHUB_OUTPUT"
     echo "LWC_NAME_CHECK=$LWC_NAME_CHECK" >> "$GITHUB_OUTPUT"
 else
-    echo -e "\nINVALID LWC components name found!:\n${INVALID_FILES[*]}"
+    echo -e "\n INVALID LWC components name found!:\n${INVALID_FILES[*]}"
     LWC_NAME_CHECK="Failed!"
     echo "INVALID_FILES=${INVALID_FILES[*]}" >> "$GITHUB_OUTPUT"
     echo "LWC_NAME_CHECK=$LWC_NAME_CHECK" >> "$GITHUB_OUTPUT"
