8ca71bb40c922ee4d0ac3d2ddf5ad75e33f26dd1
mramachandrappa <maheshramachandrappa91@gmail.com>
2025-04-24 20:14:58 +0530

    Auto base branch injection

--- .github/workflows/lwc_name_check.sh
+++ .github/workflows/lwc_name_check.sh
@@ -7,7 +7,7 @@ echo "REGEX_PATTERN is defined in repository variable"
 echo "REGEX_PATTERN is $REGEX_PATTERN"
 
 # Get changed .cls files
-FILES_CHANGED=($(git diff --name-only --diff-filter=A origin/main...HEAD -- 'lwc/*'))
+FILES_CHANGED=($(git diff --name-only --diff-filter=A origin/${{ github.event.pull_request.base.ref }}...HEAD -- 'lwc/*'))
 
 echo -e "Found the following lwc components with changes:\n${FILES_CHANGED[*]}\n"
 
