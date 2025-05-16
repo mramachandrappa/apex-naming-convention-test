678a71b8ddf636c1ba10fbf99192ab2dea587074
mramachandrappa <maheshramachandrappa91@gmail.com>
2025-05-16 18:48:39 +0530

    SKIP failure test

--- .github/workflows/scripts/naming_convention_check.sh
+++ .github/workflows/scripts/naming_convention_check.sh
@@ -43,7 +43,7 @@ else
     echo "INVALID_FILES=${INVALID_FILES[*]}" >> "$GITHUB_OUTPUT"
     echo "NAME_CHECK=$NAME_CHECK" >> "$GITHUB_OUTPUT"
     
-    if [ "$ENFORCE" = false ]; then
+    if [ "$ENFORCE" = True ]; then
         exit 1
     fi
 fi
