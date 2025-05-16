a6d69e950bb483128bbe5f9bcce6146a51fde36d
mramachandrappa <maheshramachandrappa91@gmail.com>
2025-05-16 18:45:57 +0530

    SKIP failure test

--- .github/workflows/scripts/naming_convention_check.sh
+++ .github/workflows/scripts/naming_convention_check.sh
@@ -42,7 +42,8 @@ else
     NAME_CHECK="Failed!"
     echo "INVALID_FILES=${INVALID_FILES[*]}" >> "$GITHUB_OUTPUT"
     echo "NAME_CHECK=$NAME_CHECK" >> "$GITHUB_OUTPUT"
-    if !$SKIP_FAILURE; then
+    
+    if [ "$SKIP_FAILURE" = false ]; then
         exit 1
     fi
 fi
