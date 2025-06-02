8e0a7b825d5f6b2e6096f4e59c320421da7acbc9
Mahesh Ramachandrappa <maheshramachandrappa91@gmail.com>
2025-06-02 18:40:43 +0530

    Common shell script (#18)
    * Moved workflows to use common script
    
    * Added lwc folder
    
    * Added lwc folder
    
    * Added lwc folder
    
    * test

--- .github/workflows/scripts/naming_convention_check.sh
+++ .github/workflows/scripts/naming_convention_check.sh
@@ -8,7 +8,7 @@ echo "REGEX_PATTERN is $REGEX_PATTERN"
 echo "BASE_REF is $BASE_REF"
 
 # Get changed .object files
-FILES_CHANGED=($(git diff --name-only --diff-filter=A $BASE_REF...HEAD -- "$FOLDER_NAME/*"))
+FILES_CHANGED=($(git diff --name-only --diff-filter=AM $BASE_REF...HEAD -- "$FOLDER_NAME/*"))
 
 if [[ ${#FILES_CHANGED[@]} -eq 0 ]]; then
     echo "No files found with changes"
@@ -22,7 +22,9 @@ echo -e "Files detected for naming convention check:\n${FILES_CHANGED[*]}\n"
 # Validate naming conventions
 for FILE_PATH in "${FILES_CHANGED[@]}"; do
     FILE_NAME=$(basename "$FILE_PATH")
-    matched=false
+    if [ "$FOLDER_NAME" = "lwc" ]; then
+        FILE_NAME=$(echo "$FILE_PATH" | cut -d'/' -f2)  
+    fi
 
     if echo "$FILE_NAME" | grep -Eq "$REGEX_PATTERN"; then
         echo "$FILE_NAME passed naming convention check."
@@ -31,13 +33,6 @@ for FILE_PATH in "${FILES_CHANGED[@]}"; do
         echo "$FILE_NAME did NOT pass naming convention check!"
     fi
 done
-
-if [ "$ENFORCE" = true ]; then
-    echo "MESSAGE=Kindly update $FOLDER_NAME as per naming convention defined." >> "$GITHUB_OUTPUT"
-    exit 1
-else
-    echo "MESSAGE=Hey! You probably updated $FOLDER_NAME name incorrectly. Please check!" >> "$GITHUB_OUTPUT"
-fi
 
 # Output results
 if [[ ${#INVALID_FILES[@]} -eq 0 ]]; then
@@ -50,4 +45,11 @@ else
     NAME_CHECK="Failed!"
     echo "INVALID_FILES=${INVALID_FILES[*]}" >> "$GITHUB_OUTPUT"
     echo "NAME_CHECK=$NAME_CHECK" >> "$GITHUB_OUTPUT"
+    
+    if [ "$ENFORCE" = true ]; then
+        echo "MESSAGE=Kindly update $FOLDER_NAME as per naming convention defined." >> "$GITHUB_OUTPUT"
+        exit 1
+    else
+        echo "MESSAGE=Hey! You probably updated $FOLDER_NAME name incorrectly. Please check!" >> "$GITHUB_OUTPUT"
+    fi
 fi
